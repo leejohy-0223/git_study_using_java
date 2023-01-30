e45a6431fc1d7ec6f3c1ec1443752e3988de65a7
leejohy-0223 <leejohy@naver.com>
2023-01-30 22:12:41 +0900

    feat: initial commit

---
+++ src/main/java/com/example/git_study_using_java/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.git_study_using_java;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
